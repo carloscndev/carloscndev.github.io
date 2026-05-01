e1317cc80c5e18f28462f5fbe0becad482758d9f
carloscncdev <carloscndev@gmail.com>
2026-05-01 16:11:48 -0600

    feat: Adding education section

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -4,6 +4,7 @@ import ImageProfile from '../ImageProfile/ImageProfile';
 import JobTitle from '../JobTitle/JobTitle';
 import AboutMe from '../AboutMe/AboutMe';
 import Skills from '../Skills/Skills';
+import Education from '../Education/Education';
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <main className="main-content">
         <AboutMe />
         <Skills />
+        <Education />
       </main>
       <div className="social-sidebar">
           Icons
---
+++ src/Components/Education/Education.tsx
@@ -0,0 +1,38 @@
+import BlockSection from "../BlockSection/BlockSection";
+
+interface EducationItemProps {
+  title: string;
+  period: string;
+  institution: string;
+}
+const Education = () => (
+  <BlockSection 
+   title="Education"
+   children={<EducationContent />}
+  />
+);
+
+const EducationContent = () => (
+  <>
+    <EducationItem 
+      title="Mechatronic Engineering Degree"
+      period="2008 - 2013"
+      institution="Instituto Politécnico Nacional, Gustavo A. Madero, CDMX, México"
+    />
+     <EducationItem 
+      title="Computer Science Studies (Relevant Coursework)"
+      period="2015 - 2023"
+      institution="Universidad Tecmilenio, Santa Barbara, Azcapotzalco, CDMX, México"
+    />
+  </>
+);
+
+const EducationItem = ({ title, period, institution }: EducationItemProps) => (
+  <div className="education-item">
+    <p><strong>{title}</strong></p>
+    <p><i>{period}</i></p>
+    <p>{institution}</p>
+  </div>
+);
+
+export default Education;
